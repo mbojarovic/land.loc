704980765c866122a7956b10cf325fff56a086e7
Marek <m.bojarovic@gmail.com>
2017-02-16 21:12:23 +0200

    Final D8 update

--- resources/views/admin/portfolios.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('Users.Marek.Downloads.ishodniki-landing.resources.views.layouts.admin')
-
-@section('header')
-	@include('Users.Marek.Downloads.ishodniki-landing.resources.views.admin.header')
-@endsection
-
-@section('content')
-	@include('admin.content_portfolios')
-@endsection
